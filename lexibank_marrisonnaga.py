df3ee860e82afe0e2840f8158a50bae6be5dd6be
chrzyki <christoph@functor-argument.net>
2019-11-11 13:35:43 +0100

    Refactorings, release prep, added tests

--- lexibank_marrisonnaga.py
+++ lexibank_marrisonnaga.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank import Language
 from pylexibank import FormSpec
-from pylexibank.util import pb
+from pylexibank.util import progressbar
 from clldutils.misc import slug
 import lingpy
 import attr
+
 
 @attr.s
 class CustomLanguage(Language):
@@ -17,14 +18,12 @@ class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Area = attr.ib(default=None)
 
+
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "marrisonnaga"
     language_class = CustomLanguage
-    form_spec = FormSpec(
-            missing_data=("*", "---", ""),
-            brackets={"[": "]", "(": ")"}
-            )
+    form_spec = FormSpec(missing_data=("*", "---", ""), brackets={"[": "]", "(": ")"})
 
     def cmd_makecldf(self, args):
         """
@@ -32,16 +31,15 @@ class Dataset(BaseDataset):
         """
         wl = lingpy.Wordlist(self.raw_dir.joinpath("GEM-CNL.csv").as_posix())
         concept_lookup = args.writer.add_concepts(
-                id_factory=lambda x: x.id.split('-')[-1]+'_'+slug(x.english),
-                lookup_factory="Name"
-                )
-        language_lookup = args.writer.add_languages(
-                lookup_factory="STEDT_Name")
+            id_factory=lambda x: x.id.split("-")[-1] + "_" + slug(x.english), lookup_factory="Name"
+        )
+        language_lookup = args.writer.add_languages(lookup_factory="STEDT_Name")
         args.writer.add_sources()
         # check for missing items
         missing = defaultdict(int)
-        for idx, language, concept, value, pos in pb(
-            wl.iter_rows("doculect", "concept", "reflex", "gfn")):
+        for idx, language, concept, value, pos in progressbar(
+            wl.iter_rows("doculect", "concept", "reflex", "gfn")
+        ):
             if concept not in concept_lookup:
                 if pos == "n":
                     if concept + " (noun)" in concept_lookup:
@@ -60,5 +58,5 @@ class Dataset(BaseDataset):
                     Language_ID=language_lookup[language],
                     Parameter_ID=concept_lookup[concept],
                     Value=value,
-                    Source=["Marrison1967"]
+                    Source=["Marrison1967"],
                 )
